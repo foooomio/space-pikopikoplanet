10ea1b3162af83923347a1666e70d217aad62d67
foooomio <foooomio@gmail.com>
2020-09-22 03:28:14 +0900

    Delete unnecessary file

--- components/query/list.js
+++ components/query/list.js
@@ -1,19 +1,20 @@
+import { useSWRInfinite } from 'swr';
 import { Button, Card, Divider, Loader } from 'semantic-ui-react';
 import QueryCard from '@/components/query/card';
-import { useInfiniteScroll } from '@/lib/use-infinite-scroll';
 import { NUMBER_IN_QUERY_LIST } from '@/lib/constants';
 
 export default function QueryList({ getKey, fetcher }) {
-  const {
-    data,
-    size,
-    setSize,
-    isLoadingMore,
-    isEmpty,
-    isReachingEnd,
-  } = useInfiniteScroll(getKey, fetcher, {}, NUMBER_IN_QUERY_LIST);
+  const { data, error, size, setSize } = useSWRInfinite(getKey, fetcher);
 
   const queries = data?.flat() ?? [];
+
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore =
+    isLoadingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === 'undefined');
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty || (data && data[data.length - 1]?.length < NUMBER_IN_QUERY_LIST);
 
   return (
     <>
