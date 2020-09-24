f2ddf64111554ea704f136424c81d124fbd0b08e
foooomio <foooomio@gmail.com>
2020-09-24 21:35:57 +0900

    Make endpoint selectable

--- components/sparql/endpoint-input.js
+++ components/sparql/endpoint-input.js
@@ -1,18 +1,34 @@
-import { forwardRef } from 'react';
+import { useState, useEffect, forwardRef } from 'react';
 import { Input } from 'semantic-ui-react';
+import { fetchEndpointList } from '@/lib/database';
 
 const SparqlEndpointInput = forwardRef((props, ref) => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    if (props.defaultValue) return;
+    fetchEndpointList().then((data) => setList(data));
+  }, []);
+
   return (
-    <Input
-      type="url"
-      placeholder="Endpoint"
-      icon="compass outline"
-      iconPosition="left"
-      fluid
-      transparent
-      {...props}
-      ref={ref}
-    />
+    <>
+      <Input
+        type="url"
+        list="endpoint-list"
+        placeholder="Endpoint"
+        icon="compass outline"
+        iconPosition="left"
+        fluid
+        transparent
+        {...props}
+        ref={ref}
+      />
+      <datalist id="endpoint-list">
+        {list.map((endpoint) => (
+          <option value={endpoint} key={endpoint} />
+        ))}
+      </datalist>
+    </>
   );
 });
 
