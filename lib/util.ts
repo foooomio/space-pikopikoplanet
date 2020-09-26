cb2b09dc2b2d13e88339971ed1e1f9cbf8956255
foooomio <foooomio@gmail.com>
2020-09-26 20:47:10 +0900

    Fix isUrl

--- lib/util.ts
+++ lib/util.ts
@@ -6,11 +6,5 @@ export const generateId = (): string => Math.random().toString(16).slice(-10);
 export const doubleEscape = (str: string): string =>
   encodeURIComponent(encodeURIComponent(str));
 
-export const isUrl = (str: string): boolean => {
-  try {
-    new URL(str);
-    return true;
-  } catch {
-    return false;
-  }
-};
+export const isUrl = (str: string): boolean =>
+  str.startsWith('http://') || str.startsWith('https://');
