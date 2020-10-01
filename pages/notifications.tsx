3c2b0682fc96eacf86629eefea4c7abf5266c5c9
foooomio <foooomio@gmail.com>
2020-10-02 00:53:51 +0900

    Fix notifications page

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,9 +1,18 @@
+import { useRouter } from 'next/router';
 import { Segment, Container, Header } from 'semantic-ui-react';
 import Layout from '@/components/common/layout';
 import Head from '@/components/common/head';
 import NotificationFeed from '@/components/notification/feed';
+import { useUser } from '@/hooks/use-user';
 
 const NotificationsPage = () => {
+  const [user, loading] = useUser();
+  const router = useRouter();
+
+  if (typeof window !== 'undefined' && !loading && !user) {
+    router.replace('/sign-in');
+  }
+
   return (
     <Layout>
       <Head subtitle="通知" />
